bd207b469b5e380a832d83a4661da2133dbbbf52
bechtify <34157267+bechtify@users.noreply.github.com>
2018-06-13 15:33:19 +0200

    Add files via upload
    Beim Speichern eines neuen Spielers wird nun eine Datei erstellt, welche die Standardwerte und den übergebenen Namen enthält.
    Dies ist per Klassen und Methodenaufruf des Data Managers möglich

--- project/PlayerCreation.java
+++ project/PlayerCreation.java
@@ -8,6 +8,7 @@ import javax.swing.*;
 //import com.jgoodies.forms.layout.RowSpec;
 import java.awt.event.ActionListener;
 import java.io.FileReader;
+import java.io.IOException;
 import java.awt.event.ActionEvent;
 import java.awt.Font;
 
@@ -59,13 +60,9 @@ public class PlayerCreation {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 
-		JButton btnNewButton = new JButton("Save");
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		btnNewButton.setBounds(45, 218, 117, 29);
-		frame.getContentPane().add(btnNewButton);
+		JButton btnSave = new JButton("Save");
+		btnSave.setBounds(45, 218, 117, 29);
+		frame.getContentPane().add(btnSave);
 
 		JLabel lblUsername = new JLabel("Enter Player Name:");
 		lblUsername.setFont(new Font("Tahoma", Font.PLAIN, 20));
@@ -84,11 +81,19 @@ public class PlayerCreation {
 		});
 		frame.getContentPane().add(btnDelete);
 
-		JTextField nameField = new JTextField("test");
+		JTextField nameField = new JTextField("Enter Name");
 		nameField.setHorizontalAlignment(SwingConstants.CENTER);
 		nameField.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		nameField.setVisible(true);
 		nameField.setBounds(200, 22, 220, 41);
+		btnSave.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(nameField.getText()!= ("Enter Name")) {			//File for a new player gets added to folder "players"
+					DataManager.addPlayer(nameField.getText());		//Adds Standard Values for players
+					DataManager.getPlayer(nameField.getText());		//Just for directly testing @Simon
+				}
+			}
+		});
 		frame.getContentPane().add(nameField);
 		
 
@@ -104,7 +109,7 @@ public class PlayerCreation {
 				
 		
 		
-		ImageIcon icon = createImageIcon("sonne.jpg");
+		ImageIcon icon = createImageIcon("sonne.jpg");							//Parameter ändern @Simon
 		JLabel label1 = new JLabel("Image and Text", icon, JLabel.CENTER);
 		label1.setBounds(255,84,110, 110);
 		
