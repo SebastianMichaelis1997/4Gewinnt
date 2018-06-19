ee8e912ec036a19c1ba15f92dd16215bbd7e3a3d
bechtify <34157267+bechtify@users.noreply.github.com>
2018-06-19 11:18:15 +0200

    Add files via upload
    Change Propertys möglich -> ändern des Namens ändert das attribut name und den Namen des Files. Achtung! Für andere attribute (wins etc. ) noch nicht getestet!!!

--- project/DataManager.java
+++ project/DataManager.java
@@ -5,15 +5,18 @@ import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class DataManager {
 
 	public static String[] getPlayer(String filename) {
 		try {
-			FileReader fr = new FileReader(System.getProperty("user.dir") + "\\src\\players\\" + filename + ".player"); //@Simon noch zu bearbeiten
+			FileReader fr = new FileReader(System.getProperty("user.dir") + "\\src\\players\\" + filename + ".player");
 			BufferedReader br = new BufferedReader(fr);
 			String[] playerData = new String[8];
-			for (int i = 0; i < playerData.length; i++) {	//reads all lines of player file
+			for (int i = 0; i < playerData.length; i++) { // reads all lines of player file
 				playerData[i] = br.readLine();
 				System.out.println(playerData[i]);
 			}
@@ -25,23 +28,69 @@ public class DataManager {
 		return null;
 	}
 
-	public static void addPlayer(String text){
+	public static void addPlayer(String name) {
 		try {
-			FileWriter fw = new FileWriter(System.getProperty("user.dir") + "\\src\\players\\" + text + ".player");
+			FileWriter fw = new FileWriter(System.getProperty("user.dir") + "\\src\\players\\" + name + ".player");
 			BufferedWriter bw = new BufferedWriter(fw);
 
-			bw.write(text);
+			bw.write(name);
 			bw.newLine();
-			for (int i = 0; i < 5; i++) {	//Standard values
+			for (int i = 0; i < 5; i++) { // Standard values
 				bw.write(0);
 				bw.newLine();
 			}
-			bw.write("null");		//Standard value
+			bw.write("null"); // Standard value
 			bw.newLine();
-			bw.write("red");		//Standard value
+			bw.write("red"); // Standard value
 			bw.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
+
+	public static void changeProperty(String player, String attribute, String value) {
+		try {
+			String[] playerData = getPlayer(player);
+			FileWriter fw = new FileWriter(System.getProperty("user.dir") + "\\src\\players\\" + value + ".player");
+			BufferedWriter bw = new BufferedWriter(fw);
+			for (int i = 0; i < playerData.length; i++) {
+				if ((attribute == "name") && (i == 0)) {
+					bw.write(value);
+					bw.newLine();
+				} else if ((attribute == "nrOfGames") && (i == 1)) {
+					bw.write(value);
+					bw.newLine();
+				} else if ((attribute == "wins") && (i == 2)) {
+					bw.write(value);
+					bw.newLine();
+				} else if ((attribute == "losses") && (i == 3)) {
+					bw.write(value);
+					bw.newLine();
+				} else if ((attribute == "ties") && (i == 4)) {
+					bw.write(value);
+					bw.newLine();
+				} else if ((attribute == "score") && (i == 5)) {
+					bw.write(value);
+					bw.newLine();
+				} else if ((attribute == "icon") && (i == 6)) {
+					bw.write(value);
+					bw.newLine();
+				} else if ((attribute == "color") && (i == 7)) {
+					bw.write(value);
+					bw.newLine();
+				} else {
+					bw.write(playerData[i]);
+					bw.newLine();
+				}
+			} // end for
+			bw.close();
+			if (attribute == "name") {
+				Path path = (Paths.get(System.getProperty("user.dir") + "\\src\\players\\" + player + ".player"));
+				Files.delete(path);
+			}
+		} // end try
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+	}// end change property
 }
