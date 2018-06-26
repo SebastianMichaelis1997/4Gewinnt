a7ca5e06e526a58c8320d4179b934872379b6add
bechtify <34157267+bechtify@users.noreply.github.com>
2018-06-26 10:32:23 +0200

    Update ReadMeWindow.java
    Layout geändert

--- project/ReadMeWindow.java
+++ project/ReadMeWindow.java
@@ -1,29 +1,19 @@
 package project;
 
 import javax.swing.*;
+import javax.swing.border.EtchedBorder;
+import javax.swing.border.TitledBorder;
 
-import java.awt.*;
-import java.awt.event.*;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class ReadMeWindow {
-	private JFrame f; // Main frame
-	private JTextArea ta; // Text area
-	private JScrollPane sbrText; // Scroll pane for text area
-	private JButton btnClose; // Close Frame
+public class ReadMeWindow extends JFrame {
 
-	public ReadMeWindow() { // Constructor
-		// Create Frame
-		f = new JFrame("ReadMe");
-		f.getContentPane().setLayout(new FlowLayout());
-
-		// Get Content from ReadMeFile
+	public ReadMeWindow() {
 		String text = "";
 		try {
-			FileReader fr = new FileReader(System.getProperty("user.dir")
-					+ "\\src\\project\\ReadMe");
+			FileReader fr = new FileReader(System.getProperty("user.dir")+"\\src\\project\\ReadMe");
 			BufferedReader br = new BufferedReader(fr);
 			String sCurrentLine;
 			while ((sCurrentLine = br.readLine()) != null) {
@@ -34,40 +24,29 @@ public class ReadMeWindow {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		// System.out.println(text);
+		JPanel middlePanel = new JPanel();
+		// middlePanel.setBorder(new TitledBorder(new EtchedBorder(), "Display Area"));
 
-		// Create Scrolling Text Area
-		ta = new JTextArea(text, 16, 26);
-		ta.setLineWrap(true);
+		// create the middle panel components
 
-		ta.setEditable(false);
-		sbrText = new JScrollPane(ta);
-		sbrText.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+		JTextArea display = new JTextArea();
+		display.setText(text);
+		display.setEditable(false); // set textArea non-editable
+		JScrollPane scroll = new JScrollPane(display);
+		scroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		display.grabFocus();
+		display.setCaretPosition(20);
 
-		// Create Close Button
-		btnClose = new JButton("Zurück");
-		btnClose.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				f.dispose();
-			}
-		});
-		this.launchFrame();
+		// Add Textarea in to middle panel
+		middlePanel.add(scroll);
+
+		// My code
+		JFrame frame = new JFrame();
+		frame.add(middlePanel);
+		frame.pack();
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
 	}
 
-	public void launchFrame() { // Create Layout
-		// Add text area and button to frame
-		f.getContentPane().add(sbrText);
-		f.getContentPane().add(btnClose);
-
-		// Close when the close button is clicked
-		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-		// Display Frame
-		f.pack(); // Adjusts frame to size of components
-		f.setVisible(true);
-	}
-
-	// public static void main(String args[]) {
-	// ReadMe2 gui = new ReadMe2();
-	// gui.launchFrame();
-	// }
 }
