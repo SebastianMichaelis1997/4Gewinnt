f4c617c88a780c6362f146f1a410f8aec9869c7d
Eneszaza21 <39615351+Eneszaza21@users.noreply.github.com>
2018-06-29 11:14:50 +0200

    Umlaute hinzugefügt

--- project/ReadMe.java
+++ project/ReadMe.java
@@ -3,18 +3,18 @@ package project;
 public class ReadMe {
 	public static final String README ="Version 1.0 22/06/2019\r\n" + 
 			"\r\n" + 
-			"4-Gewinnt-Projekt von Alexander Dreher, Enes Akgümüs, Tobias Rothley, Emma Falldorf, Simon Becht, Sebastian Michaelis\r\n" + 
+			"4-Gewinnt-Projekt von Alexander Dreher, Enes Akg\u00fcm\u00fcs, Tobias Rothley, Emma Falldorf, Simon Becht, Sebastian Michaelis\r\n" + 
 			"\r\n" + 
-			"Tool für Mockups: Pencil: https://portableapps.com/apps/graphics_pictures/pencil-project-portable\r\n" + 
-			"JTabbedpane für Hauptmenü: https://docs.oracle.com/javase/tutorial/uiswing/components/tabbedpane.html\r\n" + 
+			"Tool fÃ¼r Mockups: Pencil: https://portableapps.com/apps/graphics_pictures/pencil-project-portable\r\n" + 
+			"JTabbedpane fÃ¼r HauptmenÃ¼: https://docs.oracle.com/javase/tutorial/uiswing/components/tabbedpane.html\r\n" + 
 			"\r\n" + 
 			"GENERAL USAGE NOTES\r\n" + 
 			"\r\n" + 
-			"Dieses PvP/PCvC wurde von unserem Team für euch erstellt. Es funktioniert auf der Basis des bekannten Spiels \"4 Gewinnt\":\r\n" + 
-			"Jeder Spieler erhält 21 frei konfigurierbare Spielsteine. Die beiden Spieler dürfen immer nur ein Spielstein auf einmal setzen.\r\n" + 
-			"Nach jedem Zug ist der andere Spieler an der Reihe. Bei jedem Wurf in eine der 7 Spalten fällt der Spielstein in das unterste freie Feld. \r\n" + 
-			"Das Ziel dieses Spiels ist es, vier Steine in einer zusammenhÃ¤ngenden Reihe anzuordnen, sei es horizontal, diagonal oder vertikal. \r\n" + 
-			"Beim erreichen dieses Ziels erhält der Spieler ein Punkt. Sollte aber keiner der Spieler nach dem legen seiner 21 Steine gewonnen haben,\r\n" + 
+			"Dieses PvP/PCvC wurde von unserem Team f\u00fcr euch erstellt. Es funktioniert auf der Basis des bekannten Spiels \"4 Gewinnt\":\r\n" + 
+			"Jeder Spieler erhÃ¤lt 21 frei konfigurierbare Spielsteine. Die beiden Spieler d\u00fcrfen immer nur ein Spielstein auf einmal setzen.\r\n" + 
+			"Nach jedem Zug ist der andere Spieler an der Reihe. Bei jedem Wurf in eine der 7 Spalten f\u00e4llt der Spielstein in das unterste freie Feld. \r\n" + 
+			"Das Ziel dieses Spiels ist es, vier Steine in einer zusammenh\u00e4ngenden Reihe anzuordnen, sei es horizontal, diagonal oder vertikal. \r\n" + 
+			"Beim erreichen dieses Ziels erh\u00e4lt der Spieler ein Punkt. Sollte aber keiner der Spieler nach dem legen seiner 21 Steine gewonnen haben,\r\n" + 
 			"endet das Spiel in einem Unentschieden.\r\n" + 
 			"\r\n" + 
 			"INSTALLATION GUIDE \r\n" + 
