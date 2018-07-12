2d4741e7c8eeb10e60cc5f1a8a7af8168f512005
SebastianMichaelis1997 <sebastian@sileachim.de>
2018-07-12 10:17:30 +0200

    Update ReadMe.java

--- project/ReadMe.java
+++ project/ReadMe.java
@@ -8,8 +8,17 @@ public class ReadMe {
 			"Tool für Mockups: Pencil: https://portableapps.com/apps/graphics_pictures/pencil-project-portable\r\n" + 
 			"JTabbedpane für Hauptmenü: https://docs.oracle.com/javase/tutorial/uiswing/components/tabbedpane.html\r\n" + 
 			"\r\n" + 
+			"INSTRUCTIONS"
+			"Wenn auf dem Main Window bist hast du oben in der Leiste Tabs mit denen du in die Spieler einstellungen kommst und auf die d Spiel einstellungen.\r\n" +
+			"Im Fenster Player hast du die möglichkeit die Spieler anzulegen, zu bearbeiten und zu löschen.\r\n" +
+			"Im Fenster Optionen hast du die gelegenheit dieses ReadMe hier zu lesen, aber das sollte dir klar sein wenn du gerade dabei bist dies hier zu lesen.\r\n" +
+			"Zusätzlich kannst du noch einstellungen am Sound übernehmen.\r\n" +
+			"Wenn du oben wieder auf den Tab 'Game' klickst dann hast du die Optionen die Spieler auszuwählen, entweder gegen den Computer oder gegen einen anderen Spieler.\r\n" +
+			"Nach dem du das Spiel gestartet hast du dann das Spielfeld links, die Statistiken, dazu wer gerade dran ist, rechts und oben links in der leiste 2 Tabs\r\n" +
+			"Mit dem Tab 'Optionen' kannst du ein neues Spiel starten oder es beenden.\r\n" +
+			"Mit dem Tab 'Hilfe' kanns du dir hilfe holen"
 			"GENERAL USAGE NOTES\r\n" + 
-			"\r\n" + 
+			"\r\n" +
 			"Dieses PvP/PCvC wurde von unserem Team f\u00fcr euch erstellt. Es funktioniert auf der Basis des bekannten Spiels \"4 Gewinnt\":\r\n" + 
 			"Jeder Spieler erhält 21 frei konfigurierbare Spielsteine. Die beiden Spieler d\u00fcrfen immer nur ein Spielstein auf einmal setzen.\r\n" + 
 			"Nach jedem Zug ist der andere Spieler an der Reihe. Bei jedem Wurf in eine der 7 Spalten f\u00e4llt der Spielstein in das unterste freie Feld. \r\n" + 
