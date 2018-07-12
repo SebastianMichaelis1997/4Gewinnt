9f45985fb6897517d3b6150ed0d35b6c87e27af5
bechtify <34157267+bechtify@users.noreply.github.com>
2018-07-12 11:26:06 +0200

    formatted readme
    by Michaelis

--- project/ReadMe.java
+++ project/ReadMe.java
@@ -1,38 +1,28 @@
 package project;
 
 public class ReadMe {
-	public static final String README ="Version 1.0 22/06/2019\r\n" + 
-			"\r\n" + 
-			"4-Gewinnt-Projekt von Alexander Dreher, Enes Akg\u00fcm\u00fcs, Tobias Rothley, Emma Falldorf, Simon Becht, Sebastian Michaelis\r\n" + 
-			"\r\n" + 
-			"Tool für Mockups: Pencil: https://portableapps.com/apps/graphics_pictures/pencil-project-portable\r\n" + 
-			"JTabbedpane für Hauptmenü: https://docs.oracle.com/javase/tutorial/uiswing/components/tabbedpane.html\r\n" + 
-			"\r\n" + 
-			"INSTRUCTIONS"
-			"Wenn auf dem Main Window bist hast du oben in der Leiste Tabs mit denen du in die Spieler einstellungen kommst und auf die d Spiel einstellungen.\r\n" +
-			"Im Fenster Player hast du die möglichkeit die Spieler anzulegen, zu bearbeiten und zu löschen.\r\n" +
-			"Im Fenster Optionen hast du die gelegenheit dieses ReadMe hier zu lesen, aber das sollte dir klar sein wenn du gerade dabei bist dies hier zu lesen.\r\n" +
-			"Zusätzlich kannst du noch einstellungen am Sound übernehmen.\r\n" +
-			"Wenn du oben wieder auf den Tab 'Game' klickst dann hast du die Optionen die Spieler auszuwählen, entweder gegen den Computer oder gegen einen anderen Spieler.\r\n" +
-			"Nach dem du das Spiel gestartet hast du dann das Spielfeld links, die Statistiken, dazu wer gerade dran ist, rechts und oben links in der leiste 2 Tabs\r\n" +
-			"Mit dem Tab 'Optionen' kannst du ein neues Spiel starten oder es beenden.\r\n" +
-			"Mit dem Tab 'Hilfe' kanns du dir hilfe holen"
-			"GENERAL USAGE NOTES\r\n" + 
-			"\r\n" +
-			"Dieses PvP/PCvC wurde von unserem Team f\u00fcr euch erstellt. Es funktioniert auf der Basis des bekannten Spiels \"4 Gewinnt\":\r\n" + 
-			"Jeder Spieler erhält 21 frei konfigurierbare Spielsteine. Die beiden Spieler d\u00fcrfen immer nur ein Spielstein auf einmal setzen.\r\n" + 
-			"Nach jedem Zug ist der andere Spieler an der Reihe. Bei jedem Wurf in eine der 7 Spalten f\u00e4llt der Spielstein in das unterste freie Feld. \r\n" + 
-			"Das Ziel dieses Spiels ist es, vier Steine in einer zusammenh\u00e4ngenden Reihe anzuordnen, sei es horizontal, diagonal oder vertikal. \r\n" + 
-			"Beim erreichen dieses Ziels erh\u00e4lt der Spieler ein Punkt. Sollte aber keiner der Spieler nach dem legen seiner 21 Steine gewonnen haben,\r\n" + 
-			"endet das Spiel in einem Unentschieden.\r\n" + 
-			"\r\n" + 
-			"INSTALLATION GUIDE \r\n" + 
-			"\r\n" + 
-			"It is recommended to use WIN7/WIN8.1/WIN1/macOSHighsierra V.10.13.5 or the latest verion ///// /////\r\n" + 
-			"\r\n" + 
-			"HELP EMAIL: sew-enes@hotmail.com\r\n" + 
-			"\r\n" + 
-			"Copyright 2017-2018. All reights reserved. Programm should only be used with a valid license from one of the team member.\r\n" + 
-			"All brand and product names are traddemarks or registered."
-			+ "";
+	public static final String README = "Version 1.0 22/06/2019\r\n" + "\r\n"
+			+ "4-Gewinnt-Projekt von Alexander Dreher, Enes Akg\u00fcm\u00fcs, Tobias Rothley, Emma Falldorf, Simon Becht, Sebastian Michaelis\r\n"
+			+ "\r\n"
+			+ "Tool für Mockups: Pencil: https://portableapps.com/apps/graphics_pictures/pencil-project-portable\r\n"
+			+ "JTabbedpane für Hauptmenü: https://docs.oracle.com/javase/tutorial/uiswing/components/tabbedpane.html\r\n"
+			+ "\r\n" + "INSTRUCTIONS"
+			+ "Wenn auf dem Main Window bist hast du oben in der Leiste Tabs mit denen du in die Spieler einstellungen kommst und auf die d Spiel einstellungen.\r\n"
+			+ "Im Fenster Player hast du die möglichkeit die Spieler anzulegen, zu bearbeiten und zu löschen.\r\n"
+			+ "Im Fenster Optionen hast du die gelegenheit dieses ReadMe hier zu lesen, aber das sollte dir klar sein wenn du gerade dabei bist dies hier zu lesen.\r\n"
+			+ "Zusätzlich kannst du noch einstellungen am Sound übernehmen.\r\n"
+			+ "Wenn du oben wieder auf den Tab 'Game' klickst dann hast du die Optionen die Spieler auszuwählen, entweder gegen den Computer oder gegen einen anderen Spieler.\r\n"
+			+ "Nach dem du das Spiel gestartet hast du dann das Spielfeld links, die Statistiken, dazu wer gerade dran ist, rechts und oben links in der leiste 2 Tabs\r\n"
+			+ "Mit dem Tab 'Optionen' kannst du ein neues Spiel starten oder es beenden.\r\n"
+			+ "Mit dem Tab 'Hilfe' kanns du dir hilfe holen" + "GENERAL USAGE NOTES\r\n" + "\r\n"
+			+ "Dieses PvP/PCvC wurde von unserem Team f\u00fcr euch erstellt. Es funktioniert auf der Basis des bekannten Spiels \"4 Gewinnt\":\r\n"
+			+ "Jeder Spieler erhält 21 frei konfigurierbare Spielsteine. Die beiden Spieler d\u00fcrfen immer nur ein Spielstein auf einmal setzen.\r\n"
+			+ "Nach jedem Zug ist der andere Spieler an der Reihe. Bei jedem Wurf in eine der 7 Spalten f\u00e4llt der Spielstein in das unterste freie Feld. \r\n"
+			+ "Das Ziel dieses Spiels ist es, vier Steine in einer zusammenh\u00e4ngenden Reihe anzuordnen, sei es horizontal, diagonal oder vertikal. \r\n"
+			+ "Beim erreichen dieses Ziels erh\u00e4lt der Spieler ein Punkt. Sollte aber keiner der Spieler nach dem legen seiner 21 Steine gewonnen haben,\r\n"
+			+ "endet das Spiel in einem Unentschieden.\r\n" + "\r\n" + "INSTALLATION GUIDE \r\n" + "\r\n"
+			+ "It is recommended to use WIN7/WIN8.1/WIN1/macOSHighsierra V.10.13.5 or the latest verion ///// /////\r\n"
+			+ "\r\n" + "HELP EMAIL: sew-enes@hotmail.com\r\n" + "\r\n"
+			+ "Copyright 2017-2018. All reights reserved. Programm should only be used with a valid license from one of the team member.\r\n"
+			+ "All brand and product names are traddemarks or registered." + "";
 }
