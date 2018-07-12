b1f1eb5301795ff4f1f7a9696af2c013d7ee2e6d
bechtify <34157267+bechtify@users.noreply.github.com>
2018-07-12 13:59:11 +0200

    Doppelte Datein gelöscht

--- 4gewinnt/src/project/IllegalNameException.java
+++
@@ -1,7 +0,0 @@
-package project;
-
-public class IllegalNameException extends Exception {
-	public IllegalNameException(String illegalName) {
-		super("Name darf nicht \"" + illegalName + "\" sein");
-	}
-}
--- 4gewinnt/src/project/PlayerAlreadyExistsException.java
+++
@@ -1,9 +0,0 @@
-package project;
-
-public class PlayerAlreadyExistsException extends Exception {
-
-	public PlayerAlreadyExistsException(String name, String additionalMsg) {
-		super();
-		ErrorWindow.start("<html><body>Player with name "+name+" already exists!<br> Names are not case sensitive!<br> Please choose another name!<br>"+additionalMsg+"</body></html>");
-	}
-}
