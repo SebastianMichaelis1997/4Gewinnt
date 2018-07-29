a9c722392a91b219a3a73dc8bff2efb2d1e90b52
bechtify <34157267+bechtify@users.noreply.github.com>
2018-07-29 22:04:21 +0200

    Fehlermeldung verbessert

--- 4-Gewinnt/src/exceptions/ExceptionPvP.java
+++ 4-Gewinnt/src/exceptions/ExceptionPvP.java
@@ -3,7 +3,7 @@ package exceptions;
 public class ExceptionPvP extends Exception {
 
 	public ExceptionPvP() {
-		super("<html><body>Du kannst nicht gegen dich selbst spielen!<br>Such bitte einen anderen Spieler 2 aus.</body></html>");
+		super("<html><body>Du kannst nicht gegen dich selbst spielen!<br>Wähle bitte einen anderen Spieler 2.</body></html>");
 
 	}
 
